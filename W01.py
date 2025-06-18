e3d6be8992fe6a5bbff6742126738bd0fd1370c6
peetel22 <peetel22@hotmail.com>
2025-06-18 18:29:21 +0700

    first commit

---
+++ W01.py
@@ -0,0 +1,22 @@
+# print ("Hello Onit")
+
+# ตัวแปรจำนวนเต็ม
+number_int = 13
+# ตัวแปรทศนิยม
+number_float = 3.14
+
+# ตัวแปรข้อความ
+txt = "Nitipoom Phantong"
+
+#  บูลีน true/False
+is_online = True
+Power_on = False
+emergency_mode = True
+
+print(number_float)
+print(number_int)
+print(txt)
+print(type(number_float))
+print(type(number_int))
+print(type(is_online))
+print(type(emergency_mode))
